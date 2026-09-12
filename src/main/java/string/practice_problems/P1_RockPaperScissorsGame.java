58ad7657fccae78e9264948962542e859daa6a92
dev-gracy <gracyyyy.g@gmail.com>
2026-09-12 13:32:27 +0530

    Add P1 Rock Paper Scissors Game

---
+++ src/main/java/string/practice_problems/P1_RockPaperScissorsGame.java
@@ -0,0 +1,82 @@
+package string.practice_problems;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class P1_RockPaperScissorsGame {
+
+    static String playRound(String playerMove, String computerMove) {
+
+        if (playerMove.equalsIgnoreCase(computerMove)) {
+            return "Draw";
+        }
+
+        if ((playerMove.equalsIgnoreCase("Rock") && computerMove.equals("Scissors")) ||
+            (playerMove.equalsIgnoreCase("Paper") && computerMove.equals("Rock")) ||
+            (playerMove.equalsIgnoreCase("Scissors") && computerMove.equals("Paper"))) {
+            return "Player Wins";
+        }
+
+        return "Computer Wins";
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] moves = {"Rock", "Paper", "Scissors"};
+
+        int rounds = 5;
+        int wins = 0;
+        int losses = 0;
+        int draws = 0;
+
+        String[] playerMoves = new String[rounds];
+        String[] computerMoves = new String[rounds];
+        String[] results = new String[rounds];
+
+        for (int i = 0; i < rounds; i++) {
+
+            System.out.print("Enter your move (Rock/Paper/Scissors): ");
+            String playerMove = scanner.nextLine();
+
+            String computerMove = moves[random.nextInt(3)];
+
+            String result = playRound(playerMove, computerMove);
+
+            playerMoves[i] = playerMove;
+            computerMoves[i] = computerMove;
+            results[i] = result;
+
+            if (result.equals("Player Wins")) {
+                wins++;
+            } else if (result.equals("Computer Wins")) {
+                losses++;
+            } else {
+                draws++;
+            }
+        }
+
+        System.out.println("\nRound | Player Move | Computer Move | Result");
+
+        for (int i = 0; i < rounds; i++) {
+
+            System.out.println(
+                (i + 1) + " | " +
+                playerMoves[i] + " | " +
+                computerMoves[i] + " | " +
+                results[i]
+            );
+        }
+
+        double winPercentage = (wins * 100.0) / rounds;
+
+        System.out.println("\nWins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Draws: " + draws);
+        System.out.println("Win % = " + winPercentage + "%");
+
+        scanner.close();
+    }
+}
