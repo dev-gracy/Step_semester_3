7d24f79927aaed9ec67a5f403df1d8786ae61c5c
dev-gracy <gracyyyy.g@gmail.com>
2026-09-12 13:15:02 +0530

    Add Q3 Course Credit Management

---
+++ src/main/java/string/assignment_problems/Q3_CourseCreditManagement.java
@@ -0,0 +1,36 @@
+package string.assignment_problems;
+
+class Course {
+
+    String code;
+    String title;
+    int credits;
+    int labCredits;
+
+    Course(String code, String title, int credits, int labCredits) {
+        this.code = code;
+        this.title = title;
+        this.credits = credits;
+        this.labCredits = labCredits;
+    }
+
+    Course(String code, String title, int credits) {
+        this(code, title, credits, 0);
+    }
+
+    int totalCredits() {
+        return credits + labCredits;
+    }
+}
+
+public class Q3_CourseCreditManagement {
+
+    public static void main(String[] args) {
+
+        Course course1 = new Course("21CSC201J", "Programming", 3, 1);
+        Course course2 = new Course("21CSC205L", "Data Structures", 4);
+
+        System.out.println(course1.code + " total credits: " + course1.totalCredits());
+        System.out.println(course2.code + " total credits: " + course2.totalCredits());
+    }
+}
